b3c6ada3d5ed6ba405b9664cc579994f857cb3ac
ahmed <ahmedelsharabasy2004@gmail.com>
2025-11-04 14:37:38 +0200

    Add purchase model with schema definition for supplier, products, total amount, and date

---
+++ models/purchaseModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const purchaseSchema = new mongoose.Schema(
+  {
+    supplier: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true } //for create createdAt,updatedAt and update the date automaticlly in updatedAt prb
+);
+
+const Purchase = mongoose.model("Purchase", purchaseSchema);
+module.exports = Purchase;
